1eb545b13037eeba2bb9b57ea88cf96b346f45b5
David Hallin <53200282+hallindavid@users.noreply.github.com>
2025-10-24 08:26:04 -0600

    wip - removed the migrations for the ES stuff

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -6,6 +6,7 @@ use Database\Factories\CardFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Card extends Model
@@ -19,7 +20,7 @@ class Card extends Model
         'user_uuid'
     ];
 
-    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class, 'user_uuid','uuid');
     }
