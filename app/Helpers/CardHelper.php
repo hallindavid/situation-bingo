6d3e0ecc14455b5990d9e7050adcfc8daed6b05c
David Hallin <53200282+hallindavid@users.noreply.github.com>
2025-10-25 14:00:53 -0600

    WIP

--- app/Helpers/CardHelper.php
+++ app/Helpers/CardHelper.php
@@ -6,6 +6,7 @@ use App\Models\Card;
 use App\Models\CardSituation;
 use App\Models\Situation;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 class CardHelper
 {
@@ -30,17 +31,24 @@ class CardHelper
         Card::all()->each(function (Card $card) {
             for ($i = 1; $i <= 16; $i++) {
 
-                if ($card->cardSituations()->where('card_position', $i)->count() === 0) {
+                $cardSituation = $card->cardSituations()->where('card_position', $i)->first();
+
+                if ($cardSituation === null) {
                     // If the card situation doesn't exist, create it with null values.
+
+                    $selected_situation = Situation::availableForCard($card)->inRandomOrder()->first();
+
                     $card->cardSituations()->create([
                         'card_position' => $i,
-                        'situation_uuid' => null,
+                        'situation_uuid' => $selected_situation->uuid,
                         'situation_occurrence_uuid' => null,
                     ]);
+                } elseif ($cardSituation->situation_uuid === null) {
+                    $selected_situation = Situation::availableForCard($card)->inRandomOrder()->first();
+                    $cardSituation->situation_uuid = $selected_situation->uuid;
+                    $cardSituation->save();
                 }
-
             }
-
         });
     }
 
@@ -51,6 +59,8 @@ class CardHelper
 
     public function generateSituationsForCard(Card $card): void
     {
+        DB::table('card_situations')->where('card_uuid', $card->uuid)->update(['situation_uuid' => null]);
+
         $cardSituations = $card->cardSituations()->where('situation_uuid', null)->get();
 
         $cardSituations->each(function (CardSituation $cardSituation) {
